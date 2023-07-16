87369bf573f378abe13714e082a0aa9031e336a3
lakshya <lakshyaroy848@gmail.com>
2023-07-16 15:32:51 +0530

    footer error solved

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -5,19 +5,6 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="footer">
-          <Typography.Title
-            level={5}
-            style={{ color: "white", textAlign: "center" }}
-          >
-            CryptBlaze <br />
-            Copyrights 2023 ©️ All Rights Reserved
-          </Typography.Title>
-          <Space>
-            <Link to="/">Home</Link>
-            <Link to="/exchanges">Exchanges</Link>
-            <Link to="/news">News</Link>
-            <Link to="/about">About</Link>
-          </Space>
       <Typography.Title
         level={5}
         style={{ color: "white", textAlign: "center" }}
